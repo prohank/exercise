60b780321ecd8d700093c3315c3dc4cb9d33ab03
prohank <rohan.patel002@gmail.com>
2020-05-10 00:03:43 +0530

    Solved one more competitive problem, Added other solution for StockMaxProfit.java

---
+++ src/competitive/leetcode/april/GroupAnagrams.java
@@ -0,0 +1,76 @@
+package competitive.leetcode.april;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        GroupAnagrams ga = new GroupAnagrams();
+        ga.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}).forEach(System.out::println);
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> sortedStrings = new HashMap<>();
+        List<String> strings;
+        for(String s: strs){
+            char[] sorted = s.toCharArray();
+            Arrays.sort(sorted);
+            String sortedString = String.valueOf(sorted);
+            if(sortedStrings.containsKey(sortedString)){
+                List<String> value = sortedStrings.get(sortedString);
+                value.add(s);
+                strings = value;
+            } else {
+                strings = new ArrayList<>();
+                strings.add(s);
+            }
+            sortedStrings.put(sortedString,strings);
+        }
+        return new ArrayList<>(sortedStrings.values());
+    }
+
+
+//////////////////////////////////////////////////////////////////
+//    public List<List<String>> groupAnagrams(String[] strs) {
+//        List<List<String>> anagrams = new ArrayList<>();
+//        Set<String> stringSet = new HashSet<>();
+//        for (int i = 0; i < strs.length; i++) {
+//            List<String> strings = new ArrayList<>();
+//            String primary = null;
+//            if (!stringSet.contains(strs[i])) {
+//                for (int j = i; j < strs.length; j++) {
+//                    if (strings.isEmpty()) {
+//                        primary = strs[j];
+//                        strings.add(strs[j]);
+//                        stringSet.add(strs[j]);
+//                    } else if (areAnagrams(primary, strs[j])) {
+//                        strings.add(strs[j]);
+//                        stringSet.add(strs[j]);
+//                    }
+//                }
+//            }
+//            if (!strings.isEmpty()) anagrams.add(strings);
+//        }
+//        return anagrams;
+//    }
+//
+//    private boolean areAnagrams(String s1, String s2) {
+//        if (s1 == null || s2 == null) return false;
+//        char[] s1Arr = s1.toLowerCase().toCharArray(), s2Arr = s2.toLowerCase().toCharArray();
+//        if (s1Arr.length != s2Arr.length)
+//            return false;
+//        int[] counts = new int[26]; // An array to hold the number of occurrences of each character
+//        for (int i = 0; i < s1Arr.length; i++) {
+//            counts[s1Arr[i] - 97]++;  // Increment the count of the character at i
+//            counts[s2Arr[i] - 97]--;  // Decrement the count of the character at i
+//        }
+//        // If the strings are anagrams, the counts array will be full of zeros
+//        for (int i = 0; i < 26; i++)
+//            if (counts[i] != 0)
+//                return false;
+//        return true;
+//    }
+}
--- src/competitive/leetcode/april/StockMaxProfit.java
+++ src/competitive/leetcode/april/StockMaxProfit.java
@@ -40,3 +40,17 @@ public class StockMaxProfit {
         return sum;
     }
 }
+
+/* Other Solutions:
+
+public int maxProfit(int[] prices) {
+    int max_profit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                max_profit += prices[i] - prices[i-1];
+            }
+        }
+        return max_profit;
+    }
+
+*/
