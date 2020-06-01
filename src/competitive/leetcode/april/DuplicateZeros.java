ce1c85956c1069cd2b80cdc93c24e2893ad6a4f6
prohank <rohan.patel002@gmail.com>
2020-06-02 01:30:01 +0530

    Solved DuplicateZeros problem

---
+++ src/competitive/leetcode/april/DuplicateZeros.java
@@ -0,0 +1,27 @@
+package competitive.leetcode.april;
+
+import java.util.Arrays;
+
+
+/**
+ * https://leetcode.com/problems/duplicate-zeros/
+ */
+
+public class DuplicateZeros {
+    public static void main(String[] args) {
+        DuplicateZeros dz = new DuplicateZeros();
+        dz.duplicateZeros(new int[]{1, 0, 2, 3, 0, 4, 5, 0});
+        dz.duplicateZeros(new int[]{1, 2, 3});
+    }
+
+    public void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == 0) {
+                i++;
+                if (arr.length - i >= 0) System.arraycopy(arr, i - 1, arr, i, arr.length - i);
+
+            }
+        }
+//        System.out.println(Arrays.toString(arr));
+    }
+}
